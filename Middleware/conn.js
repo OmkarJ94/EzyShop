33f9d0e0854df900facabef50393082afb189361
OmkarJ94 <ojadhav512@gmail.com>
2023-02-06 16:07:43 +0530

    updated

--- Middleware/conn.js
+++ Middleware/conn.js
@@ -1,7 +1,5 @@
 
 import mongoose from 'mongoose';
-//URI=mongodb://localhost:27017/CODESWEAR
-//  URI=mongodb+srv://omkar:Omkar%40123@cluster0.w9wbo.mongodb.net/CODESWEAR?retryWrites=true&w=majority
 mongoose.connect(process.env.URI).then(() => {
     console.log('connect')
 })
